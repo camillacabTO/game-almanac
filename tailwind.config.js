19ad4707020b9175a5d4d4d384ae9ff791716f2f
Camila <camillacab@hotmail.com>
2023-03-07 11:48:02 -0500

    installed tailwind and daisy

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        fluid: 'repeat(auto-fit, minmax(15rem, 1fr)',
+      },
+      fontFamily: {
+        monda: ['var(--font-monda)'],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['night'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'night',
+  },
+}
